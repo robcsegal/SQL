241d4db4306802e7b8200a7caeabcf4e963d0b46
Rob Segal <robcsegal@outlook.com>
2018-10-31 15:25:21 -0600

    Added constraints to sql server query.

--- PM-MVC/pm-mvc db creation 1 SSMS.sql
+++ PM-MVC/pm-mvc db creation 1 SSMS.sql
@@ -2,11 +2,19 @@
 USE [ProjectManagement]
 GO
 
-CREATE TABLE dbo.Project
+CREATE TABLE dbo.project
 (
-	project_id int not null identity(1,1),
-	description nvarchar(255) not null default(''),
+	project_id uniqueidentifier primary key,
+	[description] nvarchar(255) not null,
 	active bit not null,
-	created_date datetime not null default(getdate())
+	created_date datetime not null 
 )
 GO
+
+alter table project
+add constraint project_created_date_default default getdate() for created_date
+go
+
+alter table project
+add constraint project_description_default default '' for [description]
+go
